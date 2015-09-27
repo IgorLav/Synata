a0d2f376ec14573e8f48e9506ed58192ffde8cc6
iLavs <lamourskij@gmail.com>
2015-09-27 17:27:50 +0300

    working js offer's block

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,7 +1,27 @@
 $(document).ready( function(){
-    
+  
+  //offers blocks tabs  
    $(".section-bottom .icon-wrap").click(function(){
    	   $(".section-bottom div").removeClass('active');
    	   $(this).parent('div').toggleClass('active');
-   });
+
+   	   if ($(".mobile").hasClass("active")){
+   	   		$(".arr-bottom-section span").removeClass("active");
+   	   		$(".mobile-arr").addClass("active");
+   	   } 
+   	   else if ($(".web-app").hasClass("active")){
+   	   		$(".arr-bottom-section span").removeClass("active");
+   	   		$(".web-app-arr").addClass("active");
+   	   } 
+   	   else if ( $(".zend").hasClass("active") ){
+   	   		$(".arr-bottom-section span").removeClass("active");
+   	   		$(".zendesk-arr").addClass("active");
+		}  
+		else if( $(".service-cloud").hasClass("active") ){
+			$(".arr-bottom-section span").removeClass("active");
+			$(".service-cloud-arr").addClass("active");
+		}   		
+   	});
+   
 });
+
